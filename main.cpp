c4df8a1d360cb0486d94c3302fddfe4131dbd20c
Tiago Costa <tafcosta89@gmail.com>
2023-08-27 15:22:46 +0200

    fixed bug in do_timestepping

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ ofstream plotFile(plot_filename);
 
 int main(){
   
-  int nx {16};
-  int ny {16};
+  int nx {64};
+  int ny {64};
   int nsteps {1000};
 
-  double J {1};
-  double temp {0.0};
+  double J {0.1};
+  double temp {0.6};
   
   vector<vector<int>> spin_grid = initialise_grid(nx, ny);
   
@@ -40,8 +40,8 @@ void print_grid_values(vector<vector<int>> grid, int nx, int ny)
 void do_timestepping(int nsteps, vector<vector<int>>& grid, int nx, int ny, double J, double temp)
 {
   int istep {0};
-  vector<vector<int>> grid_tmp = grid;
-    
+  vector<vector<int>> grid_tmp(ny, vector<int>(nx, 0));
+  
   random_device rd;
   mt19937 gen(rd());
 
@@ -49,13 +49,17 @@ void do_timestepping(int nsteps, vector<vector<int>>& grid, int nx, int ny, doub
   
   while(istep < nsteps){
 
+    for(int i = 0; i < ny; i++)
+      for(int j = 0; j < nx; j++)
+	grid_tmp[i][j] = grid[i][j];
+    
     for(int i = 1; i < (ny - 1); i++)
       for(int j = 1; j < (nx - 1); j++){
 	
 	double energy = -J * grid_tmp[i][j] * (grid_tmp[i-1][j] + grid_tmp[i+1][j] + grid_tmp[i][j-1] + grid_tmp[i][j+1]);
 	double flip_energy_change = -2. * energy;
 
-	if((flip_energy_change < 0) || (distribution(gen) < exp(-flip_energy_change/temp)))
+	if((flip_energy_change < 0) || ((flip_energy_change > 0) && (distribution(gen) < exp(-flip_energy_change/temp))))
 	  grid[i][j] *= -1;
       }
 
@@ -96,7 +100,7 @@ void saveGridToFile(const vector<vector<int>>& grid, int step) {
         return;
     }
 
-    plotFile << "plot " << filename << " matrix w image\n";
+    plotFile << "plot \"" << filename << "\" matrix w image\n";
     for (const auto& row : grid) {
         for (int value : row) {
             outputFile << value << " ";
