0971494845367048c3f2ae73306e2ce8e3da9d55
Tiago Costa <tafcosta89@gmail.com>
2023-08-30 17:32:35 +0200

    more modular version

--- main.cpp
+++ main.cpp
@@ -6,115 +6,94 @@
 
 using namespace std;
 
-vector<vector<int>> initialise_grid(int nx, int ny);
-void do_timestepping(int nsteps, vector<vector<int>>& grid, int nx, int ny, double J, double temp);
-void saveGridToFile(const vector<vector<int>>& grid, int step);
-int grid_bc(int i, int j);
-
 string plot_filename = "plot.txt";
 ofstream plotFile(plot_filename);
-
-int nx {256};
-int ny {256};
 
 class Grid
 {
 public:
-  Grid(int Nside) : Ncell {Nside}, spin(Nside, vector<int>(Nside, 0))}{}  
-private:
-  vector<vector<int>> spin;
-  int Ncell;
-}
+  Grid(int Nside) : Ncell {Nside}, grid_(Nside, vector<int>(Nside)){
 
-int main(){
-  int nsteps {1000000};
-
-  double J {0.1};
-  double temp {0.2};
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> distribution(0, 1);
     
-  do_timestepping(nsteps, grid, nx, ny, J, temp);
-}
-
-void do_timestepping(int nsteps, vector<vector<int>>& grid, int nx, int ny, double J, double temp)
-{
-  int istep {0};
-  
-  random_device rd;
-  mt19937 gen(rd());
-
-  uniform_real_distribution<double> distribution(0., 1.);
-  
-  while(istep < nsteps){
-
-    int i = distribution(gen) * nx;
-    int	j = distribution(gen) * ny;
-
-    double energy = -J * grid_bc(i,j) * (grid_bc(i-1,j) + grid_bc(i+1,j) + grid_bc(i,j-1) + grid_bc(i,j+1));
-    double flip_energy_change = -2 * energy;
-
-    if((flip_energy_change < 0) || ((flip_energy_change > 0) && (distribution(gen) < exp(-flip_energy_change/temp))))
-      grid[i][j] *= -1;
-
-    if(istep % 1000 == 0)
-      saveGridToFile(grid, istep);
-    istep++;
+    for (int i = 0; i < Nside; i++)
+	for (int j = 0; j < Nside; j++)
+	  grid_[i][j] = distribution(gen) > 0.5 ? 1 : -1;
   }
-}
-
-int grid_bc(int i, int j)
-{  
-  if(i < 0)
-    i += ny;
-  else if(i >= ny)
-    i -= ny;
-
-  if(j < 0)
-    j += nx;
-  else if(j >= nx)
-    j -= nx;
   
-  return grid[i][j];
-}
-
-vector<vector<int>> initialise_grid(int nx, int ny)
-{
-  vector<vector<int>> grid(ny, vector<int>(nx));
-
-  random_device rd;
-  mt19937 gen(rd());
-  
-  uniform_real_distribution<double> distribution(0., 1.);
-  
-  for(int i = 0; i < nx; i++)
-    for(int j = 0; j < ny; j++)
-      {
-      double random_number = distribution(gen);
+  void do_timestepping(int nsteps, double J, double temp)
+  {     
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> distribution(0., 1.);
+    
+    for(int istep=0; istep!=nsteps; istep++){
       
-      if(random_number > 0.5)
-	grid[i][j] =  1;
-      else
-	grid[i][j] = -1;
-      }
-  
-  return grid;
-}
+      int i = static_cast<int>(distribution(gen) * Ncell);
+      int j = static_cast<int>(distribution(gen) * Ncell);
 
-void saveGridToFile(const vector<vector<int>>& grid, int step) {
-    string filename = "grid_step_" + to_string(step) + ".txt"; 
-    ofstream outputFile(filename);
+      double flip_delta_energy = evaluate_delta_energy(J,i,j);
+      
+      if(flip_delta_energy < 0 || (flip_delta_energy > 0 && distribution(gen) < exp(-flip_delta_energy/temp)))
+	grid_[i][j] *= -1;
 
-    if (!outputFile) {
-        cerr << "Error: Could not open file for writing." << endl;
-        return;
+      if(istep % 1000 == 0)
+	saveGridToFile(grid_, istep);
     }
+  }
 
+private:
+  vector<vector<int>> grid_;
+  int Ncell;
+
+  int grid_bc(int i, int j)
+  {
+    if(i < 0)
+      i += Ncell;
+    else if(i >= Ncell)
+      i -= Ncell;
+    
+    if(j < 0)
+      j += Ncell;
+    else if(j >= Ncell)
+      j -= Ncell;
+    
+    return grid_[i][j];
+  }
+
+  double evaluate_delta_energy(double J, int i, int j){
+    return -2 * (-J * grid_bc(i,j) * (grid_bc(i-1,j) + grid_bc(i+1,j) + grid_bc(i,j-1) + grid_bc(i,j+1)));
+  }
+  
+  void saveGridToFile(const vector<vector<int>>& grid, int step) {
+    string filename = "grid_step_" + to_string(step) + ".txt";
+    ofstream outputFile(filename);
+    
+    if (!outputFile) {
+      cerr << "Error: Could not open file for writing." << endl;
+      return;
+    }
+    
     plotFile << "plot \"" << filename << "\" matrix w image\n";
     for (const auto& row : grid) {
-        for (int value : row) {
-            outputFile << value << " ";
-        }
-        outputFile << endl;
+      for (int value : row) {
+	outputFile << value << " ";
+      }
+      outputFile << endl;
     }
-
+    
     outputFile.close();
+  }
+};
+
+int main(){
+  int Nside = 256; 
+  int nsteps {1000000};
+  double J {0.1};
+  double temp {0.1};
+
+  Grid spins(Nside);
+  spins.do_timestepping(nsteps, J, temp);
 }
